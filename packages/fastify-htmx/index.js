3ad5f7da380bf944c762cea82e8fe96220144e76
Jan Monschke <janmonschke@fastmail.com>
2025-02-21 21:34:35 +0100

    chore(fastify-htmx): add fast path when extension specified

--- packages/fastify-htmx/index.js
+++ packages/fastify-htmx/index.js
@@ -152,6 +152,9 @@ async function findClientImports(
   const specifiers = (
     await Promise.all(
       findStaticImports(source).map(async ({ specifier }) => {
+        if (extname(specifier)) {
+          return specifier
+        }
         const resolved = resolve(dirname(path), specifier)
 
         // resolve the file extension which allows for
